af66e8793c2a08936867bff9f0c4ab4f0aa07a85
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-03-05 18:04:23 +0800

    get some idea of pid

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+  pid_t p = getpid();
+  printf("%d\n", p);
   assert(!argv[argc]);
   return 0;
 }
