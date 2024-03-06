f3571b405702f5d6ee5dd7107c5e5d72b15fe681
tracer-nju <tracer@nju.edu.cn>
2024-03-06 22:03:25 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:03:25 up 22 min,  1 user,  load average: 0.07, 0.06, 0.01

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_DIRS 256
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
@@ -9,6 +15,39 @@ int main(int argc, char *argv[]) {
   }
   pid_t p = getpid();
   printf("%d\n", p);
+
+  DIR *dir;
+  struct dirent *entry;
+  char *dirs[MAX_DIRS];
+  int count;
+
+  // open the directory
+  dir = opendir("/proc");
+  if (dir == NULL) {
+    perror("Error: Unable to open directory");
+    return 1;
+  }
+  while((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_DIR && entry->d_name[0] != '.') { // ignore. && ..
+      dirs[count] = malloc(strlen(entry->d_name) + 1);
+      strcpy(dirs[count], entry->d_name);
+      count++;
+    }
+    if (count >= MAX_DIRS) {
+      fprintf(stderr, "Too many directoris\n");
+      break;
+    }
+  }
+
+  closedir(dir);
+
+  for (int i = 0; i < count; i++) {
+    printf("%s ", dirs[i]);
+    free(dirs[i]);
+  }
+
+  printf("\n");
+
   assert(!argv[argc]);
   return 0;
 }
