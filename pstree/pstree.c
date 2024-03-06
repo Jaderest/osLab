79093a0d4e4842d6e183306789b1e3a48561e083
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-03-06 22:27:47 +0800

    read all process without its ppid

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX_DIRS 256
+
+int isNumeric(const char *str) {
+  while(*str) {
+    if (!isdigit(*str)) {
+      return 0;
+    }
+    str++;
+  }
+  return 1;
+}
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+
+  DIR *dir;
+  struct dirent *entry;
+  char *dirs[MAX_DIRS];
+  int count = 0;
+
+  // open the directory
+  dir = opendir("/proc");
+  if (dir == NULL) {
+    perror("Error: Unable to open directory");
+    return 1;
   }
-  pid_t p = getpid();
-  printf("%d\n", p);
+  while((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_DIR && entry->d_name[0] != '.' && isNumeric(entry->d_name)) { // ignore. && ..
+      dirs[count] = malloc(strlen(entry->d_name) + 1);
+      strcpy(dirs[count], entry->d_name);
+      count++;
+    }
+    if (count >= MAX_DIRS) {
+      fprintf(stderr, "Too many directoris\n");
+      break;
+    }
+  }
+
+  closedir(dir);
+
+  for (int i = 0; i < count; i++) {
+    printf("%s ", dirs[i]);
+    free(dirs[i]);
+  }
+
+  printf("\n");
+
   assert(!argv[argc]);
   return 0;
 }
