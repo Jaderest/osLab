6b104e09d7f2eb5e06014ccad1066c5b9df66d2d
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-03-11 15:09:54 +0800

    update to my proceed

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <assert.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define MAX_PROC 512
 
 typedef struct PROCESS {
   char name[128];
-  int pid; // pid
-  int ppid; // parents pid
+  int pid;
+  int ppid;
   struct PROCESS **children;
   int childCount;
 } Process;
@@ -16,20 +21,23 @@ int _v = 0;
 
 int isNumeric(const char *str) {
   while(*str) {
-    if (!isdigit(*str)) {
+    if(*str < '0' || *str > '9') {
       return 0;
     }
     str++;
   }
-  return 1;
-} //and now i know why my git have so many bug
+  return 2;
+}
 
-void parsePid(const char *statusPath, ProcNode *process) {
-  FILE *status = fopen(statusPath, "r");
+void parseProcess(const char *statusPath, Process *process) {
+  FILE *statusFile = fopen(statusPath, "r");
+  if(statusFile == NULL) {
+    perror("Unable to open status file");
+    return;
+  }
+
   char line[256];
-  
-  while (fgets(line, sizeof(line), status) != NULL) {
-    //finish!
+  while(fgets(line, sizeof(line), statusFile)) {
     if (strstr(line, "Pid:") != NULL) {
       sscanf(line, "Pid: %d", &process->pid);
     }
@@ -40,23 +48,22 @@ void parsePid(const char *statusPath, ProcNode *process) {
       sscanf(line, "Name: %[^\n]", process->name);
     }
   }
-  
-  fclose(status);
+
+  fclose(statusFile);
 }
 
-void findPPid(ProcNode *child, ProcNode *nodes[], int count) {
+void findPPid(Process *child, Process *nodes[], int count) {
   for (int i = 0; i < count; i++) {
     if (child->ppid == nodes[i]->pid) {
-      nodes[i]->children = (ProcNode**)realloc(nodes[i]->children, (nodes[i]->childCount + 1) * sizeof(ProcNode*));
-      nodes[i]->children[nodes[i]->childCount++] = child;
+      nodes[i]->children = (Process **)realloc(nodes[i]->children, (nodes[i]->childCount + 1) * sizeof(Process));
+      nodes[i]->children[nodes[i]->childCount] = child;
+      nodes[i]->childCount++;
       return;
     }
   }
 }
 
-char blank[MAX_DEPTH][3];
-
-void printTree(ProcNode* root, int depth) { //recursion
+void printTree(Process *root, int depth) {
   for (int i = 0; i < depth; i++) {
     printf("  ");
   }
@@ -67,46 +74,81 @@ void printTree(ProcNode* root, int depth) { //recursion
   }
 }
 
-void PrintTree(ProcNode *process[], int count) {
+int compare(const void *a, const void *b) {
+  return strcmp((*(Process **)a)->name, (*(Process **)b)->name);
+}
+
+void PrintTree(Process *process[], int count) {
   for (int i = 0; i < count; i++) { // proc1 must be the first
     findPPid(process[i], process, count);
+  }
+
+  if (_p == 1) {
+    for (int i = 0; i < count; i++) {
+      char new_name[256];
+      snprintf(new_name, sizeof(new_name), "%s(%d)", process[i]->name, process[i]->pid);
+      strcpy(process[i]->name, new_name);
+    }
+  }
+
+  if (_n == 1) ;
+  else { //sort by name
+    for (int i = 0; i < count; i++) {
+      qsort(process[i]->children, process[i]->childCount, sizeof(Process *), compare);
+    }
   }
 
   printTree(process[0], 0);
 }
 
 int main(int argc, char *argv[]) {
+  assert(argv[0]);
+  for (int i = 1; i < argc; i++) {
+    assert(argv[i]); // C 标准保证
+    if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0) {
+      _p = 1;
+    } else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "-numeric-sort") == 0) {
+      _n = 1;
+    } else if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) {
+      _v = 1;
+    } else {
+      perror("Args error");
+      return 1;
+    }
+  }
+  assert(!argv[argc]); // C 标准保证
+
+  if (_v == 1) {
+    printf("pstree 1.0\n");
+    printf("This is a minilab finished by Jaderest\n");
+    printf("He is really happy to do this as homework in OS class\n");
+    return 0;
+  }
+
   DIR *dir;
   struct dirent *entry;
-  ProcNode *process[MAX_PROC] = {NULL};
+  Process *process[MAX_PROC] = {NULL};
   int count = 0;
 
-  // open the directory
   dir = opendir("/proc");
-  if (dir == NULL) {
-    perror("Error: Unable to open directory");
-    return 1;
+  if(dir == NULL) {
+    perror("Unable to open /proc");
+    return 2;
   }
+
   while((entry = readdir(dir)) != NULL) {
-    if (entry->d_type == DT_DIR && entry->d_name[0] != '.' && isNumeric(entry->d_name)) { // ignore. && ..
-      
-      //path to status
+    if(entry->d_type == DT_DIR && entry->d_name[0] != '.' && isNumeric(entry->d_name)) {
+
       char statusPath[512];
       snprintf(statusPath, sizeof(statusPath), "/proc/%s/status", entry->d_name);
 
       process[count] = malloc(sizeof(Process));
       process[count]->pid = 0;
       process[count]->ppid = 0;
-      process[count]->children = NULL;
       process[count]->childCount = 0;
-
-      parsePid(statusPath, process[count]);
-
+      process[count]->children = NULL;
+      parseProcess(statusPath, process[count]);
       count++;
-    }
-    if (count >= MAX_PROC) {
-      fprintf(stderr, "Too many directoris\n");
-      break;
     }
   }
 
@@ -115,10 +157,7 @@ int main(int argc, char *argv[]) {
   PrintTree(process, count);
 
   for (int i = 0; i < count; i++) {
-    if (process[i] != NULL) {
-      free(process[i]);
-    }
+    free(process[i]);
   }
-
   return 0;
 }
