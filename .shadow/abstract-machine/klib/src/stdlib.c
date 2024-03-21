e29ced8d4c2bedb8bae8d6512e8d272f9c2e7792
tracer-nju <tracer@nju.edu.cn>
2024-03-21 19:34:24 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:34:24 up  2:14,  1 user,  load average: 0.08, 0.12, 0.09

--- .shadow/abstract-machine/klib/src/stdlib.c
+++ .shadow/abstract-machine/klib/src/stdlib.c
@@ -6,14 +6,6 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-typedef struct memory_block {
-  size_t size;
-  struct memory_block *next;
-} mem_block;
-// 空闲内存链表头指针
-static mem_block *free_list = NULL;
-
-
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -43,49 +35,12 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  // panic("Not implemented");
-//   mem_block *current, *prev;
-//   void *result;
-
-//   // 对齐内存块大小
-//   size = (size + sizeof(mem_block) - 1) / sizeof(mem_block) * sizeof(mem_block);
-
-//   for (current = free_list, prev = NULL; current != NULL; prev = current, current = current->next) {
-//     if (current->size >= size) {
-//       if (current->size > size + sizeof(mem_block)) { // 找到足够大的内存块
-//         mem_block *split_block = (mem_block *)((char *)current + size);
-//         split_block->size = current->size - size - sizeof(mem_block);
-//         split_block->next = current->next;
-//         if (prev == NULL) {
-//           free_list = split_block;
-//         } else {
-//           prev->next = split_block;
-//         }
-//       } else { // 使用整个内存块
-//         if (prev == NULL) {
-//           free_list = current->next;
-//         } else {
-//           prev->next = current->next;
-//         }
-//       }
-//       result = (void *)((char *)current + sizeof(mem_block));
-//       return result;
-//     }
-//     return NULL;
-//   }
+  panic("Not implemented");
 #endif
   return NULL;
 }
 
 void free(void *ptr) {
-  if (ptr == NULL) {
-    return;
-  }
-
-  mem_block *block = (mem_block *)((char *)ptr - sizeof(mem_block));
-
-  block->next = free_list;
-  free_list = block;
 }
 
 #endif
