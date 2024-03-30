21dbb1cf5e54f0d08619db94e8278832dc11e6f4
tracer-nju <tracer@nju.edu.cn>
2024-03-30 16:15:06 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:15:06 up  2:29,  1 user,  load average: 0.23, 0.17, 0.25

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -121,7 +121,7 @@ void splash() {
   draw_background(0xffffff); // 设置背景颜色
   draw_line(0, 0, w, h, 5, 0xff0000); // 画一条线
   draw_line(w, 0, 0, h, 5, 0x00ff00);
-  fill_triangle((Point){0, 0}, (Point){w/2, 0}, (Point){w/3, h}, 0x0000ff); // 填充三角形
+  fill_triangle((Point){0, h/2}, (Point){w/2, 0}, (Point){w/3, h}, 0x0000ff); // 填充三角形
 }
 
 // Operating system is a C program!
