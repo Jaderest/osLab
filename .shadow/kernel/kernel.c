eb3dc73fb3be3a307297e9898959c33e552e00e8
tracer-nju <tracer@nju.edu.cn>
2024-03-30 18:52:16 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:52:16 up  4:24,  1 user,  load average: 0.51, 0.57, 0.38

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -202,7 +202,7 @@ void splash43(int w, int h) {
   draw_line((Point){0, 0}, (Point){w / 2, 2 * h}, 1, 0x0000ff); // blue
   draw_line((Point){w, 0}, (Point){w / 2, 2 * h}, 1, 0x00ff00); // green
   fill_triangle((Point){w / 4, h/2}, (Point){w/2, 0}, (Point){3*w/4, h/2}, 0x008b8b); // darkcyan
-  fill_triangle((Point){w / 4, h/8}, (Point){w/2, 5*h/8}, (Point){3*w/4, h/8}, 0x00ffff); // aqua
+  fill_triangle((Point){w / 4, 3*h/16}, (Point){w/2, 11*h/16}, (Point){3*w/4, 3*h/16}, 0x00ffff); // aqua
   fill_circle(w / 2, h, 50, 0xff0000); // red
 }
 
