46a7f67ad70bdbdfcc698b4f1011c7e55effdab9
tracer-nju <tracer@nju.edu.cn>
2024-04-07 15:53:53 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:53:53 up 25 min,  1 user,  load average: 0.08, 0.11, 0.09

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct co { // 需要分配内存，可以使用malloc
+typedef struct co { // 需要分配内存，可以使用malloc，自己设计这个结构体
     void (*func)(void *);
     void *arg;
     char name[16];
@@ -17,8 +17,12 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     return NULL;
 }
 
-void co_wait(struct co *co) {
+void co_wait(struct co *co) { // 当前协程需要等待，知道co协程执行完成才能继续执行
+    // 被等待的协程结束后，co_wait() 返回前，co_start 分配的struct co需要被释放
+    // 因此每个 协程只能被 co_wait 一次
 }
 
 void co_yield() {
+    // 实现协程的切换，当前协程放弃执行，若系统有多个可以运行的协程，选择一个继续执行（包括当前协程）
+    // main 函数的执行也是一个协程，因此在main中调用 co_yield() 或 co_wait() 时，程序结束
 }
