0dffac7e64e2039d64822856e3a7b9ff4c8184a1
tracer-nju <tracer@nju.edu.cn>
2024-04-07 15:42:21 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:42:21 up 13 min,  1 user,  load average: 0.26, 0.21, 0.09

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -1,10 +1,19 @@
 #include "co.h"
 #include <stdlib.h>
+#include <string.h>
 
-struct co {
-};
+typedef struct co { // 需要分配内存，可以使用malloc
+    void (*func)(void *);
+    void *arg;
+    char name[16];
+} Co;
 
+// 创建一个新的协程，返回一个指向struct co的指针
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+    Co *co = malloc(sizeof(Co));
+    co->func = func;
+    strcpy(co->name, name);
+    strcpy(co->arg, arg);
     return NULL;
 }
 
