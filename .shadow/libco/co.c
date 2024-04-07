b92b1255aaab9ccddb529d01009aec599b723adb
tracer-nju <tracer@nju.edu.cn>
2024-04-07 16:08:20 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:08:20 up 39 min,  1 user,  load average: 0.01, 0.06, 0.08

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -6,20 +6,26 @@ typedef struct co { // 需要分配内存，可以使用malloc，自己设计这
     void (*func)(void *);
     void *arg;
     char name[16];
+    // 每个协程想要执行，就需要拥有独立的堆栈和寄存器，一个协程的寄存器、堆栈、共享内存就构成了当前协程的状态机执行
 } Co;
 
 // 创建一个新的协程，返回一个指向struct co的指针
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+    // co_start() 在共享内存中创建一个新的状态机（堆栈和寄存器也保存在共享内存中）
+    // 新状态机 %rsp 寄存器指向它独立的堆栈，%rip 寄存器指向 func 函数的地址
+    //! 寄存器再看看呢
     Co *co = malloc(sizeof(Co));
     co->func = func;
     strcpy(co->name, name);
     co->arg = arg;
     return NULL;
+    // 当前代码一直执行下去是什么意思
 }
 
 void co_wait(struct co *co) { // 当前协程需要等待，知道co协程执行完成才能继续执行
     // 被等待的协程结束后，co_wait() 返回前，co_start 分配的struct co需要被释放
     // 因此每个 协程只能被 co_wait 一次
+    // 等待状态机结束，即等待 func 函数执行完成
 }
 
 void co_yield() {
