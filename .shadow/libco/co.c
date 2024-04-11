383a95554efcbfd63ab6da17507d0e7aef1194e9
tracer-nju <tracer@nju.edu.cn>
2024-04-11 14:11:06 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:11:06 up 15 min,  1 user,  load average: 0.10, 0.08, 0.02

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #ifdef LOCAL_MACHINE
     #define debug(...) printf(__VA_ARGS__)
@@ -9,8 +10,26 @@
     #define debug(...)
 #endif
 
-struct co {
+struct context {
+    // 上下文切换，把寄存器都保存下来
+};
 
+enum co_status {
+    CO_NEW = 1,
+    CO_RUNNING,
+    CO_WAITING,
+    CO_DEAD,
+};
+
+struct co {
+    char *name;
+    void (*func)(void *); // co_start 指定的入口地址和函数
+    void *arg;
+
+    enum co_status status;
+    struct co* waiter;
+    struct context ctx;
+    uint8_t stack[4096]; // 协程的堆栈
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
