f481df3e32196278a5788481906c48b275baa1db
tracer-nju <tracer@nju.edu.cn>
2024-04-11 14:02:59 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:02:59 up 7 min,  1 user,  load average: 0.11, 0.05, 0.01

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -9,8 +9,12 @@
     #define debug(...)
 #endif
 
+struct co {
+
+};
+
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-    
+    return NULL;
 }
 
 void co_wait(struct co *co) {
