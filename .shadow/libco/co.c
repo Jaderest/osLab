11b4fd2704d8dd58861fc826395ec0910c1e144e
tracer-nju <tracer@nju.edu.cn>
2024-04-12 19:41:10 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:41:10 up  7:04,  1 user,  load average: 0.06, 0.09, 0.08

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -13,10 +13,7 @@
 #endif
 
 #define STACK_SIZE 64 * 1024
-#define MAX_CO 128
-
-#define panic(cond, words) printf("Panic: %s\t", words); \
-assert(cond);
+#define MAX_CO 150
 
 enum co_status {
     CO_NEW = 1, // 新创建，还未执行过
@@ -24,6 +21,23 @@ enum co_status {
     CO_WAITING, // 在 co_wait 上等待
     CO_DEAD,    // 已经结束，但还未释放资源
 };
+
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
+{
+	asm volatile(
+#if __x86_64__
+		"movq %%rsp,-0x10(%0); leaq -0x20(%0), %%rsp; movq %2, %%rdi ; call *%1; movq -0x10(%0) ,%%rsp;"
+		:
+		: "b"((uintptr_t)sp), "d"(entry), "a"(arg)
+		: "memory"
+#else
+		"movl %%esp, -0x8(%0); leal -0xC(%0), %%esp; movl %2, -0xC(%0); call *%1;movl -0x8(%0), %%esp"
+		:
+		: "b"((uintptr_t)sp), "d"(entry), "a"(arg)
+		: "memory"
+#endif
+	);
+}
 
 struct co {
     char *name;
@@ -39,14 +53,55 @@ struct co {
 // 全局指针，指向当前运行的协程
 struct co* current = NULL;
 
+// 需要用一个数据结构存储所有的协程
+typedef struct co_node {
+    struct co *ptr;
+    struct co_node *next;
+} co_node; 
+
+co_node *head = NULL;
+
+void append(struct co *co) {
+    co_node *node = malloc(sizeof(co_node));
+    node->ptr = co;
+    node->next = NULL;
+    if (head == NULL) {
+        head = node;
+    } else {
+        co_node *p = head;
+        while(p->next != NULL) {
+            p = p->next;
+        }
+        p->next = node;
+    }
+}
+
+void delete(struct co *co) {
+    co_node *p = head;
+    co_node *q = NULL;
+    while(p != NULL) {
+        if (p->ptr == co) {
+            if (q == NULL) {
+                head = p->next;
+            } else {
+                q->next = p->next;
+            }
+            free(p);
+            break;
+        }
+        q = p;
+        p = p->next;
+    }
+}
+
 // 从头到尾，同时只有一个函数在被使用
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     // 创建一个新的状态机，仅此而已（堆栈和状态机保存在共享内存？）
     struct co *co = malloc(sizeof(struct co));
-    panic(co != NULL, "co malloc failed");
+    assert(co != NULL);
 
     co->name = malloc(strlen(name) + 1);
-    panic(co->name != NULL, "co->name malloc failed");
+    assert(co->name != NULL);
 
     strcpy(co->name, name);
     co->func = func;
@@ -60,13 +115,12 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 }
 
 void co_wait(struct co *co) { // 当前协程需要等待 co 执行完成
-    if (co->status == CO_DEAD) {
-        free(co->name);
-        free(co);
-        return;
+    current->status = CO_WAITING;
+    co->waiter = current;
+    while(co->status != CO_DEAD) {
+        co_yield();
     }
-    //? 怎么写啊wdnmd
-
+    current->status = CO_RUNNING;
 }
 
 
