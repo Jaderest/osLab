38015cd206b3aaae8a050f964115442f058015f8
tracer-nju <tracer@nju.edu.cn>
2024-04-12 16:37:11 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:37:11 up  5:45,  1 user,  load average: 0.20, 0.10, 0.02

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -66,17 +66,8 @@ void co_wait(struct co *co) { // 当前协程需要等待 co 执行完成
     }
     
     // //! 以下一段为乱写先跑一下的
-    if (current == NULL && co->status == CO_NEW) {
-        current = co;
-        co->status = CO_RUNNING;
-        co->func(co->arg);
-        co->status = CO_DEAD;
-        free(co->name);
-        free(co);
-        return;
-    }
-    // co->func(co->arg);
-    // co->status = CO_DEAD;
+    co->func(co->arg);
+    co->status = CO_DEAD;
     // //! -------to here--------
 
     free(co->name);
