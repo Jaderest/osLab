6e34377664926301862b02cee3f8090a11e2ca1a
tracer-nju <tracer@nju.edu.cn>
2024-04-12 10:41:45 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:41:45 up 22 min,  1 user,  load average: 0.11, 0.05, 0.01

--- .shadow/libco/tests/main.c
+++ .shadow/libco/tests/main.c
@@ -117,16 +117,30 @@ static void test_2() {
     q_free(queue);
 }
 
+void print(void *arg) {
+    const char *s = (const char*)arg;
+    printf("arg: %s\n", s);
+}
+
+static void test_3() {
+    struct co *thd1 = co_start("print-1", print, "X");
+    struct co *thd2 = co_start("print-2", print, "Y");
+
+    co_wait(thd1);
+    co_wait(thd2);
+}
+
 int main() {
     setbuf(stdout, NULL);
 
-    printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
-    test_1();
+    // printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
+    // test_1();
 
-    printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
-    test_2();
+    // printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
+    // test_2();
 
-    printf("\n\n");
+    printf("\n\nTest #3. My test to run them\n");
+    test_3();
 
     return 0;
 }
