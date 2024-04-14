c70466a58a037763b54189d9d62a64ca41231eee
tracer-nju <tracer@nju.edu.cn>
2024-04-14 16:15:53 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:15:53 up  5:00,  1 user,  load average: 0.16, 0.14, 0.11

--- .shadow/libco/tests/main.c
+++ .shadow/libco/tests/main.c
@@ -107,11 +107,11 @@ static void test_2() {
 
     printf("start producer\n");
     traverse();
-    co_wait(thd1); //其实这里两个 producer 都运行完了
+    co_wait(thd2); //其实这里两个 producer 都运行完了
 
     printf("start producer2\n");
     // traverse();
-    co_wait(thd2);
+    co_wait(thd1);
     // printf("finish producer\n");
 
 
