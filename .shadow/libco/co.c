04a8731164eb66f9297ad7f563d1e66c7a320517
tracer-nju <tracer@nju.edu.cn>
2024-04-14 18:36:59 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:36:59 up  7:21,  1 user,  load average: 0.49, 0.16, 0.10

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -188,7 +188,14 @@ void co_yield() {
     assert(current != NULL);
     debug("current: %s\n", current->name);
     printf("current: %d\n", current->status);
-    assert(current->status == CO_WAITING || current->status == CO_RUNNING);
+    if (current->status == CO_DEAD) {
+        delete(current);
+        free(current->name);
+        free(current);
+        current = NULL;
+        return;
+    }
+    // assert(current->status == CO_WAITING || current->status == CO_RUNNING);
     debug("into yield\n"); // 很明显的一个地方是，test2 consumer里面有调用co_yield，那就是哪里实现错误了
 
     int val = setjmp(current->context);
