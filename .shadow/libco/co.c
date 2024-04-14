bb1561be26c508acbde534a5075344efa1d5f9b9
tracer-nju <tracer@nju.edu.cn>
2024-04-14 14:37:17 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:37:17 up  3:21,  1 user,  load average: 0.36, 0.28, 0.19

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -197,9 +197,9 @@ void co_yield() {
 
 // 遍历当前的链表s
 void traverse() {
-    co_node *node = head; //是不是因为head的next只有它自己...然后就没跑动，那其实之前选线程的逻辑也是有问题的（链表构建问题可能是）
-    while (node->next != NULL){
-        printf("%s -> ", node->ptr->name);
-    }
-    printf("\n");
+    // co_node *node = head;
+    // while (node->next != NULL){
+    //     printf("%s -> ", node->ptr->name);
+    // }
+    // printf("\n");
 }
