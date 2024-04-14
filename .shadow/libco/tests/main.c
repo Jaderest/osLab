06edb33282e27773fe540eeb5aaaf1006eee8e36
tracer-nju <tracer@nju.edu.cn>
2024-04-14 21:01:51 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:01:51 up  9:46,  1 user,  load average: 0.20, 0.20, 0.28

--- .shadow/libco/tests/main.c
+++ .shadow/libco/tests/main.c
@@ -165,19 +165,19 @@ static void test_4() {
 int main() {
     setbuf(stdout, NULL);
 
-    // printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
-    // test_1();
+    printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
+    test_1();
 
-    // printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
-    // test_2();
+    printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
+    test_2();
+    printf("\n\n");
+
+    // printf("Test #3. My test to run them\n");
+    // test_3();
     // printf("\n\n");
 
-    printf("Test #3. My test to run them\n");
-    test_3();
-    printf("\n\n");
-
-    printf("Test #4. My test to run them\n");
-    test_4();
-    printf("\n\n");
+    // printf("Test #4. My test to run them\n");
+    // test_4();
+    // printf("\n\n");
     return 0;
 }
