47eb6f6cd7546a8fce9eeabe38ce0ebd88d1953d
tracer-nju <tracer@nju.edu.cn>
2024-04-14 16:50:02 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:50:02 up  5:34,  1 user,  load average: 1.31, 0.52, 0.32

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -161,11 +161,11 @@ void co_wait(struct co *co) { // 当前协程需要等待 co 执行完成
 co_node *choose_next() {
     co_node *node_next = head->next; // head 是 main
 
-    srand(time(NULL));
-    int random = rand() % 127;
-    for (int i = 0; i < random; i++) { // 随机化初始点
-        node_next = node_next->next;
-    }
+    // srand(time(NULL));
+    // int random = rand() % 127;
+    // for (int i = 0; i < random; i++) { // 随机化初始点
+    //     node_next = node_next->next;
+    // }
     // 事实上第一个它都没进这个循环
     while (node_next->ptr->status == CO_DEAD || node_next->ptr->status == CO_WAITING) {
         node_next = node_next->next;
