1e1a6fc7dd5fa7923c223ae01bf4928663a852f9
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-04-20 20:12:31 +0800

    rtfsc

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -19,7 +19,55 @@ void putint(int n) {
 
 // klib-macro.h提供putstr
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args; // 声明一个指向可变参数列表的对象，即参数中的...
+  va_start(args, fmt); // 宏，用于初始化va_list变量args，源是fmt
+
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+        case 'd': {
+          putint(va_arg(args, int));
+          break;
+        }
+        case 's': {
+          putstr(va_arg(args, const char *));
+          break;
+        }
+        case 'c': {
+          putch(va_arg(args, int));
+          break;
+        }
+        case 'x': {
+          putch('0');
+          putch('x');
+          int num = va_arg(args, int);
+          for (int i = 24; i >= 0; i -= 4) {
+            putch("0123456789abcdef"[(num >> i) & 0xf]); // 0xf = 0b1111，也即取出num的每4位
+          }
+          break;
+        }
+        case 'p': {
+          putch('0');
+          putch('x');
+          uintptr_t num = va_arg(args, uintptr_t);
+          for (int i = 24; i >= 0; i -= 4) {
+            putch("0123456789abcdef"[(num >> i) & 0xf]);
+          }
+          break;
+        }
+        default:
+          putch(*fmt);
+          break;
+      }
+    } else {
+      putch(*fmt);
+    }
+
+    fmt++;
+  }
+  va_end(args); // 宏，用于清理va_list变量args
+  return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -3,6 +3,10 @@
 static void *kalloc(size_t size) {
     // TODO
     // You can add more .c files to the repo.
+    if (size >= 16 * 1024 * 1024) {
+        printf("kalloc: 16MiB too large\n");
+        return NULL;
+    }
 
     return NULL;
 }
@@ -13,11 +17,12 @@ static void kfree(void *ptr) {
 }
 
 static void pmm_init() {
+    // printf("%x\n", (uintptr_t)heap.start);
+    // printf("%x\n", (uintptr_t)heap.end);
     uintptr_t pmsize = (
         (uintptr_t)heap.end
         - (uintptr_t)heap.start
-    );
-
+    ); // Area heap = {}; 然后Area里面有两个指针
     printf(
         "Got %d MiB heap: [%p, %p)\n",
         pmsize >> 20, heap.start, heap.end
@@ -29,3 +34,10 @@ MODULE_DEF(pmm) = {
     .alloc = kalloc,
     .free  = kfree,
 };
+
+
+/*
+事实上，AbstractMachine 的程序运行在 freestanding 的环境下 (操作系统上的 C 程序运行在 hosted 环境下)：The __STDC_HOSTED__ macro expands to 1 on hosted implementations, or 0 on freestanding ones. The freestanding headers are: float.h, iso646.h, limits.h, stdalign.h, stdarg.h, stdbool.h, stddef.h, stdint.h, and stdnoreturn.h. You should be familiar with these headers as they contain useful declarations you shouldn't do yourself. GCC also comes with additional freestanding headers for CPUID, SSE and such.
+
+这些头文件中包含了 freestanding 程序也可使用的声明。有兴趣的同学可以发现，可变参数经过预编译后生成了类似 __builtin_va_arg 的 builtin 调用，由编译器翻译成了特定的汇编代码。
+*/
