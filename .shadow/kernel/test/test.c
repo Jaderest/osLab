c75b5b68773019249eb7fdbca6f05592c2a110eb
tracer-nju <tracer@nju.edu.cn>
2024-05-06 20:10:57 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:10:57 up  1:44,  1 user,  load average: 0.11, 0.15, 0.08

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -4,3 +4,12 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+static void entry0(int tid) { pmm->alloc(128); }
+static void goodbye()       { printf("End.\n"); }
+int main() {
+  pmm->init();
+  for (int i = 0; i < 4; i++) {
+    thread_create(entry0, i);
+  }
+  join(goodbye);
+}
