9dca42d3d210f6636e2024fb3bb1308d5cec25c8
tracer-nju <tracer@nju.edu.cn>
2024-05-08 20:41:32 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:41:32 up  4:53,  1 user,  load average: 0.26, 0.21, 0.21

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -4,7 +4,7 @@
 
 #define AVAILABLE 1
 #define UNAVAILABLE 0
-typedef int lock_t;
+typedef int lock_t; // 命名约定：_t表示自定义的数据类型
 
 void lock(lock_t *lock) {
     while (atomic_xchg(lock, UNAVAILABLE) != UNAVAILABLE) ;
@@ -18,11 +18,11 @@ void init_lock(lock_t *lock) {
 }
 
 // 一个空闲块的结构
-typedef struct freenode_t {
-    struct freenode_t *next;
+typedef struct freeblock_t {
+    struct freeblock_t *next;
     int size;
     int magic;
-} freenode_t;
+} freeblock_t;
 
 // 每个CPU设计一个page
 typedef struct pageheader_t { // 页头
@@ -37,48 +37,50 @@ typedef struct pagefreenode_t {
     int magic;
 } pagefreenode_t;
 
-typedef struct pagelist_t {
+typedef struct pagelist_t { // 每个cpu只有一个的
     pageheader_t *first; // CPU所属的页链表
     lock_t lock;
 } pagelist_t;
 
 lock_t heap_lock;
-freenode_t *head;
+freeblock_t *head;
 
 #define PAGE_SIZE (64 * 1024)
 
 // 我们通过空闲链表分配页（每个cpu），分配页：这个cpu第一次kalloc  or  当前所需内存块大小的page没有，所以分配一个new page
-void kallocpage(pageheader_t **pagehead) { //一个page多大呢？
-    lock(&heap_lock); // 对整个heap用一把大锁
-
-    // 遍历heap，找到一个大于PAGE_SIZE的空闲块
-    freenode_t *node = head;
-    // freenode_t *prev = NULL;
-    // 找到一个大于PAGE_SIZE的空闲块
-    while (node != NULL) {
-        if (node->size >= PAGE_SIZE) {
-            break;
-        }
-        // prev = node;
-        node = node->next;
-    }
-
-    if (node != NULL) { // 要在此分配一个page了
-        // freenode_t *freenode = (void *)node + PAGE_SIZE;
-        //TODO: 这里还没想好怎么分配
-    }
+void kallocpage(pageheader_t (*pagehead)[]) {
+    //TODO：每个cpu分配页（作为缓存），然后这个cpu有许多页，通过链表来管理
 }
 
+void *kallocFast(size_t size) {
+    //TODO: 分配小内存，page中直接解决，不行就新page
+
+    return NULL;
+}
+
+void *kallocHugeMemory(size_t size) {
+    //TODO: 分配大内存，大于page的内存
+
+    return NULL;
+}
 
 static void *kalloc(size_t size) {
     if (size == 0) {
         return NULL;
     } else if (size < 16) { // 最小单元16字节
         size = 16;
-    } else if (size > PAGE_SIZE) {//16KiB大内存
+    } else if (size > PAGE_SIZE) { //16KiB大内存
         if (size > 16 * 1024 * 1024) {
             return NULL;
         } // 16MiB
+        // 大内存分配
+        size_t align = 16;
+        while (align < size) {
+            align *= 2;
+        }
+        size = align;
+        void *ret = kallocHugeMemory(size);
+        return ret;
     } else { // 16 ~ 16KiB
         size_t align = 16;
         while (align < size) {
@@ -88,7 +90,8 @@ static void *kalloc(size_t size) {
         printf("size: %d\n", size);
     }
 
-    return NULL;
+    void *ret = kallocFast(size);
+    return ret;
 }
 
 static void kfree(void *ptr) {
@@ -97,6 +100,8 @@ static void kfree(void *ptr) {
 }
 
 static void pmm_init() {
+    init_lock(&heap_lock);
+
     uintptr_t pmsize = (
         (uintptr_t)heap.end
         - (uintptr_t)heap.start
@@ -112,10 +117,3 @@ MODULE_DEF(pmm) = {
     .alloc = kalloc,
     .free  = kfree,
 };
-
-
-/*
-事实上，AbstractMachine 的程序运行在 freestanding 的环境下 (操作系统上的 C 程序运行在 hosted 环境下)：The __STDC_HOSTED__ macro expands to 1 on hosted implementations, or 0 on freestanding ones. The freestanding headers are: float.h, iso646.h, limits.h, stdalign.h, stdarg.h, stdbool.h, stddef.h, stdint.h, and stdnoreturn.h. You should be familiar with these headers as they contain useful declarations you shouldn't do yourself. GCC also comes with additional freestanding headers for CPUID, SSE and such.
-
-这些头文件中包含了 freestanding 程序也可使用的声明。有兴趣的同学可以发现，可变参数经过预编译后生成了类似 __builtin_va_arg 的 builtin 调用，由编译器翻译成了特定的汇编代码。
-*/
