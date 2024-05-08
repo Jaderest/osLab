bdcb03575939029ed8dfd8c6943c23489cb74ab4
tracer-nju <tracer@nju.edu.cn>
2024-05-08 23:19:50 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:19:50 up  7:07,  1 user,  load average: 0.17, 0.13, 0.07

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -1,22 +1,3 @@
 #include <kernel.h>
 #include <klib.h>
 #include <klib-macros.h>
-
-// #define LOCKED 0
-// #define UNLOCKED 1
-
-// typedef struct {
-//     int locked;
-// } lock_t;
-
-// void lock_init(lock_t *lock) {
-//     lock->locked = UNLOCKED;
-// }
-
-// void lock(lock_t *lock) {
-//     while (atomic_xchg(&lock->locked, LOCKED) != UNLOCKED) ;
-// }
-
-// void unlock(lock_t *lock) {
-//     atomic_xchg(&lock->locked, UNLOCKED);
-// }
