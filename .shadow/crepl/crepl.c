069b296c8522ce8a36aa2026e5eec9b4c341c440
tracer-nju <tracer@nju.edu.cn>
2024-05-14 10:47:49 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:47:49 up 55 min,  1 user,  load average: 0.25, 0.14, 0.10

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -4,115 +4,140 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+// #define DEBUG
+#ifdef DEBUG
+    #define debug(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+    #define debug(fmt, ...)
+#endif
 
 //! 禁止system和popen
 
-#define DEBUG
-
-#ifdef DEBUG
-#define debug(fmt, ...) printf(fmt, ##__VA_ARGS__)
-#else
-#define debug(fmt, ...)
-#endif
-
-void *handle[128]; // 用于存储静态库
+void *handle[128];
 int handle_len = 0;
 
-int create_file(char *file, char *file_name, char *text) { //ok!
-    char src[128];
-    strcpy(src, file);
-    int fd = mkstemp(src); // 包含了路径
-    if (fd == -1) {
-        perror("mkstemp\n");
-        return 1;
+typedef int (*func_ptr)();
+
+char dir[] = "/tmp/crepl_XXXXXX";
+
+int read_line(char *strin) {
+    printf(ANSI_COLOR_GREEN "crepl> " ANSI_COLOR_RESET);
+    char *ret = fgets(strin, 4096, stdin);
+    if (ret == NULL) {
+        return 0;
     }
-    char src_name[128];
-    strcpy(src_name, src);
-    strcat(src_name, ".c");
-    if (rename(src, src_name) == -1) {
-        perror("rename\n");
-        return 1;
-    }
-    close(fd);
-
-    strcpy(file_name, src_name);
-    return 0;
-}
-
-int write_src(char *filename, char *text) {
-    FILE *fp = fopen(filename, "w");
-    if (fp == NULL) {
-        perror("fopen\n");
-        return 1;
-    }
-
-    fprintf(fp, "%s", text);
-    fclose(fp);
-    return 0;
-}
-
-int write_expr(char *filename, char *text) {
-    FILE *fp = fopen(filename, "a");
-    if (fp == NULL) {
-        perror("fopen\n");
-        return 1;
-    }
-
-    int len = strlen(text);
-    if (text[len - 1] == '\n') {
-        text[len - 1] = '\0';
-    }
-    char expr[4096] = "int __expr_warpper() { return ";
-    strcat(expr, text);
-    strcat(expr, "; }");
-
-    fprintf(fp, "%s", expr);
-    fclose(fp);
-    return 0;
+    return 1;
 }
 
 #define FUNC 1
 #define EXPR 2
 
-int use_gcc(char *filename, char *so_name) {
-    return 0;
+void *compile(char *src, int id) { // 可以返回新创建文件的句柄，用于接下来的表达式计算
+    char file_name[4096];
+    char so_name[4096];
+    snprintf(file_name, 4096, "%s/crepl%d.c", dir, handle_len);
+    snprintf(so_name, 4096, "%s/lib%d.so", dir, handle_len);
+
+    FILE *fp = fopen(file_name, "w");
+    if (fp == NULL) {
+        perror("fopen()");
+        return NULL;
+    }
+    fprintf(fp, "%s", src);
+    fclose(fp);
+
+    char *argv[] = {"gcc", "-shared", "-fPIC", "-w", file_name, "-o", so_name, NULL};
+    pid_t pid = fork();
+    if (pid == 0) {
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        execvp("gcc", argv);
+        perror("execvp()");
+        return NULL;
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            handle[handle_len] = dlopen(so_name, RTLD_LAZY | RTLD_GLOBAL); // 无敌了RTLD_GLOBAL
+            if (handle[handle_len] == NULL) {
+                perror("dlopen()");
+                return NULL;
+            }
+            handle_len++;
+            if (id == FUNC) {
+                printf(ANSI_COLOR_CYAN "Add: " ANSI_COLOR_RESET);
+                printf("%s", src); // src自己背后会有一个换行的
+            }
+        } else {
+            printf(ANSI_COLOR_RED "Compile error\n" ANSI_COLOR_RESET);
+        }
+    }
+
+    return handle[handle_len - 1];
 }
 
-int load_handle(char *text, int id) { // id是不同调用的两种情况，FUNC和EXPR
-    /*-------names-------*/
-    char tmpfile[] = "/tmp/XXXXXX"; // 用于.c和.so文件
-    char tmpfile_name[128]; // 用于存储文件名，加上.c后缀
-    char tmpfile_so[128]; // 用于存储文件名，加上.so后缀
+void calc_expr(char *text) { // 包一下
+    char src[4096];
+    char func_name[4096];
 
-    /*-------create_file-------*/
-    if (create_file(tmpfile, tmpfile_name, text) != 0) {
-        perror("create_file");
-        return 1;
+    snprintf(func_name, 4096, "__expr_wrap_%d", handle_len);
+    int len = strlen(text);
+    if (text[len - 1] == '\n') {
+        text[len - 1] = '\0';
     }
+    snprintf(src, 4096, "int __expr_wrap_%d() { return %s; }", handle_len, text);
+    debug("src: %s\n", src);
 
-    /*-------write_code-------*/
-    if (id == FUNC) {
-        if (write_src(tmpfile_name, text) != 0) {
-            perror("write_src\n");
-            return 1;
+    void *handle = compile(src, EXPR);
+    debug("handle: %p\n", handle);
+    if (handle != NULL) {
+        debug("func_name: %s\n", func_name);
+
+        // 进程间通信
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+            perror("pipe()");
+            return;
         }
-    } else if (id == EXPR) {
-        if (write_expr(tmpfile_name, text) != 0) {
-            perror("write_expr\n");
-            return 1;
+        // 创建子进程
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork()");
+            return;
+        } else if (pid == 0) {
+            close(pipefd[0]); // close read end
+            func_ptr func = dlsym(handle, func_name);
+            int ans = func();
+            write(pipefd[1], &ans, sizeof(int));
+            close(pipefd[1]);
+            exit(0);
+        } else {
+            close(pipefd[1]); // close write end
+            int ans;
+            read(pipefd[0], &ans, sizeof(int));
+            close(pipefd[0]);
+
+            int status;
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+                printf(ANSI_COLOR_CYAN);
+                printf("(%s)", text);
+                printf(ANSI_COLOR_RESET);
+                printf(" = ");
+                printf("%d\n", ans);
+            } else {
+                printf(ANSI_COLOR_RED "Runtime error\n" ANSI_COLOR_RESET);
+            }
         }
-    } else {
-        assert(0);
     }
-
-    /*-------use_gcc-------*/
-    if (use_gcc(tmpfile_name, tmpfile_so) != 0) {
-        debug("use_gcc\n");
-        return 1;
-    }
-
-    return 0;
 }
 
 void close_handle() {
@@ -123,50 +148,27 @@ void close_handle() {
 
 int main(int argc, char *argv[]) {
     static char line[4096];
+    if (mkdtemp(dir) == NULL) { //创建临时目录
+        perror("mkdtemp()");
+        return 1;
+    }
 
     while (1) {
-        printf("crepl> ");
-        fflush(stdout);
-
-        if (!fgets(line, sizeof(line), stdin)) {
+        if (!read_line(line)) {
+            perror("fgets()");
             break;
         }
-        char line_copy[4096];
-        strcpy(line_copy, line);
-
-        char *token = strtok(line_copy, " ");
-        if (strcmp(token, "int") == 0) {
-            load_handle(line, FUNC);
-            debug("%s", line);
-        } else { // "int"会识别成这里诶
-            load_handle(line, EXPR);
-            debug("expr: %s\n", line);
+        if (strcmp(line, "exit\n") == 0) {
+            break;
+        } else if (strncmp(line, "int ", 4) == 0) { //func
+            debug("func\n");
+            compile(line, FUNC);
+        } else { //expr
+            debug("expr\n");
+            calc_expr(line);
         }
-
     }
 
     close_handle();
     return 0;
 }
-
-/*
-!获取foo函数的地址
-// 打开共享库
-handle = dlopen("libfoo.so", RTLD_LAZY);
-if (!handle) {
-    fprintf(stderr, "%s\n", dlerror());
-    return 1;
-}
-
-// 清除现有的错误
-dlerror();
-
-// 获取foo函数的地址
-提供句柄和函数名即可，所以我解析变量需要解析出函数名
-*(void **) (&foo) = dlsym(handle, "foo");
-if ((error = dlerror()) != NULL)  {
-    fprintf(stderr, "%s\n", error);
-    dlclose(handle);
-    return 1;
-}
-*/
