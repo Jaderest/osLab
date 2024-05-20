51005010318ed25fe497dd1de53072ad97893539
tracer-nju <tracer@nju.edu.cn>
2024-05-20 16:07:26 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:07:26 up  5:33,  1 user,  load average: 0.47, 0.24, 0.13

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { // 参数存在argv中
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
         printf("argv[%d] = %s\n", i, argv[i]);
