ca55f882ad64555b76910b1e2a64dda3a77c0176
tracer-nju <tracer@nju.edu.cn>
2024-05-22 19:54:02 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:54:02 up  4:18,  1 user,  load average: 0.02, 0.04, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -42,7 +42,17 @@ int main(int argc, char *argv[], char *envp[]) {
         // 执行strace，并不断输出到pipefd[1]
         // dup2(pipefd[1], STDERR_FILENO);
         //TODO: argv[1]的参数也要处理，argv[1]要考虑绝对路径和相对路径，要考虑如何搜索path
-        char *exec_argc[] = {"strace", "-T", "-ttt", argv[1], NULL};
+        //传参没有传成功
+        int exec_argc_len = 4 + argc - 2 + 1; // strace -T -ttt argv[1] argv[2] ... NULL
+        char *exec_argc[exec_argc_len];
+        exec_argc[0] = "strace";
+        exec_argc[1] = "-T";
+        exec_argc[2] = "-ttt";
+        exec_argc[3] = argv[1];
+        for (int i = 2; i < argc; i++) {
+            exec_argc[i + 2] = argv[i];
+        }
+        // char *exec_argc[] = {"strace", "-T", "-ttt", argv[1], NULL};
         char *exec_envp[] = {"PATH=/usr/bin", NULL};
         debug("execve\n");
         execve("/usr/bin/strace", exec_argc, exec_envp);//
