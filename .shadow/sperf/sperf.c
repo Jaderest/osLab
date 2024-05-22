72d5e94e44d9edcaff69283ad569bf150251688e
tracer-nju <tracer@nju.edu.cn>
2024-05-22 19:45:51 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:45:51 up  4:10,  1 user,  load average: 0.18, 0.06, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -36,16 +36,16 @@ int main(int argc, char *argv[], char *envp[]) {
     }
     if (pid == 0) { // Child
         close(pipefd[0]); // Close read end
-        close(STDERR_FILENO); // 关闭不必要的输出
+        // close(STDERR_FILENO); // 关闭不必要的输出
         close(STDOUT_FILENO);
 
         // 执行strace，并不断输出到pipefd[1]
-        dup2(pipefd[1], STDERR_FILENO);
+        // dup2(pipefd[1], STDERR_FILENO);
         //TODO: argv[1]的参数也要处理，argv[1]要考虑绝对路径和相对路径，要考虑如何搜索path
         char *exec_argc[] = {"strace", "-T", "-ttt", argv[1], NULL};
         char *exec_envp[] = {"PATH=/usr/bin", NULL};
         debug("execve\n");
-        execve("/usr/bin/strace", exec_argc, exec_envp);
+        execve("/usr/bin/strace", exec_argc, exec_envp);//
         perror("execve");
 
         exit(EXIT_SUCCESS);
