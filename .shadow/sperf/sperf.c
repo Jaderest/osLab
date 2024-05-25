9ea0675b9f9afcb7212fbb8ab6a1cc62d85f99a3
tracer-nju <tracer@nju.edu.cn>
2024-05-25 16:13:34 +0800

    Linux Jaderest-win 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:13:34 up 10:25,  1 user,  load average: 0.18, 0.14, 0.09

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -94,7 +94,7 @@ int main(int argc, char *argv[], char *envp[]) { // 参数存在argv中
 
         char line[4096];
         while (fgets(line, sizeof(line), fp) != NULL) {
-            debug("%s", line);
+            // debug("%s", line);
             regmatch_t pmatch[4];
             if (regexec(&reg, line, 4, pmatch, 0) == 0) {
                 // 好的，这里正在进行统计
