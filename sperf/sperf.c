b81622db3fcb36abafbf7734238d75fee2259bf7
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-05-25 16:55:47 +0800

    how can i do the envp

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <regex.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define DEBUG
 #ifdef DEBUG
@@ -12,97 +13,115 @@
 #define debug(fmt, ...)
 #endif
 
-#define MAX_CALLS 1024
+#define MAX_SYSCALL 2048
 
-int main(int argc, char *argv[], char *envp[]) {
+typedef struct {
+    double start_time;
+    char name[64];
+    double duration;
+} syscall_info_t; // 这个数据结构肯定不行捏，必爆内存
+
+typedef struct {
+    double total_time;
+    char name[64];
+    int count;
+} syscall_summary_t;
+
+int cmp(const void *a, const void *b) {
+    return ((syscall_summary_t *)a)->total_time < ((syscall_summary_t *)b)->total_time;
+}
+
+int main(int argc, char *argv[], char *envp[]) { // 参数存在argv中
+    /*-------prepare argvs-------*/
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
-        debug("argv[%d] = %s\n", i, argv[i]);
     }
     assert(!argv[argc]);
-    /*Init: Pipe, Environment*/
+    int strace_argc = argc - 1;
+    char **strace_argv = NULL;
+    if (strace_argc <= 0) { // 什么东西都不传给strace就报错
+        // assert(0);
+    } else {
+        strace_argc += 3; // strace -T -ttt
+        // 注意strace还要加一些别的参数，整理一下先
+        strace_argv = (char **)malloc(sizeof(char *) * (strace_argc + 1));
+        strace_argv[0] = strdup("strace");
+        strace_argv[1] = strdup("-T");
+        strace_argv[2] = strdup("-ttt");
+        for (int i = 1; i < argc; i++) { // 从3开始，复制剩下的参数
+            strace_argv[i + 2] = strdup(argv[i]);
+        }
+        strace_argv[strace_argc] = NULL; // 这是最后一个参数，以NULL结尾
+    }
+    for (int i = 0; i < strace_argc; i++) {
+        debug("strace_argv[%d] = %s\n", i, strace_argv[i]);
+        fflush(stdout);
+    }
+    /*-------fork and pipe-------*/
     int pipefd[2];
     if (pipe(pipefd) == -1) {
         perror("pipe");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
-    char *path = getenv("PATH"); // 环境变量
-    // debug("path = %s\n", path);
-    /*Fork: Creating parent and child*/
     pid_t pid = fork();
     if (pid == -1) {
         perror("fork");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
-    if (pid == 0) { // Child
-        close(pipefd[0]); // Close read end
-        // close(STDERR_FILENO); // 关闭不必要的输出
-        // close(STDOUT_FILENO);
-
-        // 执行strace，并不断输出到pipefd[1]
-        // dup2(pipefd[1], STDERR_FILENO);
-        //TODO: argv[1]的参数也要处理，argv[1]要考虑绝对路径和相对路径，要考虑如何搜索path
-        //! 传参没有传成功
-        char *exec_argc[] = {"strace", "-T", "-ttt", argv[1], NULL};
-        char *exec_envp[] = {"PATH=/usr/bin", NULL};
+    if (pid == 0) {
+        close(pipefd[0]); // close read end
+        close(STDERR_FILENO);
+        // 事实上这个dup2和>比较像，但是这个是系统调用，而>是shell的功能，都是重定向
+        // dup2(pipefd[1], STDERR_FILENO); // redirect stdout to pipe
         debug("execve\n");
-        execve("/usr/bin/strace", exec_argc, exec_envp);//
-        perror("execve");
-
-        exit(EXIT_SUCCESS);
-    } else { // Parent
-        close(pipefd[1]); // Close write end
-        char line[1024];
-        int n;
-        FILE *fp = fdopen(pipefd[0], "r");
-        while (fgets(line, sizeof(line), fp) != NULL){
-            debug("%s", line);
+        // int fd = open("/dev/null", O_WRONLY); //这样stdout就不会输出了
+        // dup2(fd, STDOUT_FILENO);
+        //TODO：以上de好环境问题再解除注释
+        /**
+         * filename：是相对于进程的当前目录
+        */
+        //TODO: envp传参
+        // ./sperf-32 /usr/find . 正常执行了是为什么，牛魔的搞不清了
+        execve("/usr/bin/strace", strace_argv, envp); // 成功传参
+        for (int i = 0; strace_argv[i] != NULL; i++) {
+            debug("strace_argv[%d] = %s\n", i, strace_argv[i]);
         }
+        debug("execve\n");
+        int fdtty = open("/dev/tty", O_WRONLY);
+        dup2(fdtty, STDERR_FILENO);
+        perror("execve"); // 果然传yes会出现问题
+    } else { // parent
+        close(pipefd[1]); // close write end
+        FILE *fp = fdopen(pipefd[0], "r");
+
+        // 编译正则表达式
+        regex_t reg;
+        const char *pattern = "([0-9]+\\.[0-9]+) ([a-zA-Z0-9_]+)\\(.*\\) = .* <([0-9]+\\.[0-9]+)>";
+        if (regcomp(&reg, pattern, REG_EXTENDED) != 0) {
+            perror("regcomp");
+            exit(1);
+        }
+
+        char line[4096];
+        while (fgets(line, sizeof(line), fp) != NULL) {
+            // debug("%s", line);
+            regmatch_t pmatch[4];
+            if (regexec(&reg, line, 4, pmatch, 0) == 0) {
+                // 好的，这里正在进行统计
+                double start_time = atof(line + pmatch[1].rm_so);
+                char name[64];
+                strncpy(name, line + pmatch[2].rm_so, pmatch[2].rm_eo - pmatch[2].rm_so);
+                name[pmatch[2].rm_eo - pmatch[2].rm_so] = '\0';
+                double duration = atof(line + pmatch[3].rm_so);
+                // 以上是匹配结果，接下来该怎么做
+
+            }
+        }
+        double duration = 0;
+        double total_time = 0;
         
-        close(pipefd[0]);
     }
 
-    // char *exec_argv[] = {"strace", "-T", "-ttt", "ls", NULL}; // 这里把ls换成argv[1]（若它是以/开头的绝对路径，则直接执行，否则在PATH中搜索），然后argv[2]是argv[1]的参数
-    // char *exec_envp[] = {"PATH=", NULL};
-    // // char *exec_envp[] = {"", NULL};
-    // execve("strace", exec_argv, exec_envp);
-    // execve("/bin/strace", exec_argv, exec_envp);
-    // execve("/usr/bin/strace", exec_argv, exec_envp);
-    // perror(argv[0]);
-    // exit(EXIT_FAILURE);
 
     return 0;
 }
-
-
-
-// 仅用execve
-// 每次输出top 5的系统调用、每个系统调用至多输出一次
-/*
-TODO：2. strace  -T 输出格式，我们将argv保存起来，传给strace
-TODO：3. 如何查找环境变量
-TODO：4. 父进程不断读取子进程的输出，直到strace结束（进程需要保持实时通信）
-子进程是要使用strace的，父进程读取strace输出，现在理一下逻辑
-传进来参数，
-1. 父进程创建子进程
-2. 子进程execve执行strace，这里的ARG是strace执行将要执行程序的参数
-3. 父进程不断读取子进程的输出，直到strace结束
-子进程：
-    execve strace
-    pipe传递信息（中间的阻塞仔细想想）
-父进程：
-    创建子进程
-    读取子进程的输出
-    解析输出
-    输出结果（时间间隔、排序）
-*/
-/*
-int main(int argc, char *argv[]) { // 参数存在argv中
-    for (int i = 0; i < argc; i++) {
-        assert(argv[i]);
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-    assert(!argv[argc]);
-    return 0;
-}
-*/
