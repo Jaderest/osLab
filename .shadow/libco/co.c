403bce6d97a444f668f8406cd211999309993548
tracer-nju <tracer@nju.edu.cn>
2024-06-22 19:01:45 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:01:45 up 22 min,  1 user,  load average: 0.06, 0.08, 0.03

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -1,13 +1,27 @@
 #include "co.h"
-#include <stdlib.h>
-#include <setjmp.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <assert.h>
 #include <time.h>
-#include <stdbool.h>
-#define CO_SIZE 200
+
+#define DEBUG
+#ifdef DEBUG
+    #define debug(...) printf(__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
+
+#define STACK_SIZE 64 * 1024
+#define MAX_CO 150
+
+enum co_status {
+    CO_NEW = 1, // 新创建，还未执行过
+    CO_RUNNING, // 已经执行过
+    CO_DEAD,    // 已经结束，但还未释放资源
+};
 
 static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
 {
@@ -25,131 +39,86 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
 #endif
 	);
 }
-enum co_status
-{
-	CO_NEW = 1,		// 新创建，还未执行过
-	CO_RUNNING = 2, // 已经执行过
-	CO_WAITING = 3, // 在 co_wait 上等待
-	CO_DEAD = 4,	// 已经结束，但还未释放资源
+
+struct co {
+    char *name;
+    void (*func)(void *); // co_start 指定的入口地址和函数
+    void *arg;
+
+    enum co_status status;
+    jmp_buf context; // 寄存器现场
+    uint8_t stack[STACK_SIZE]; // 协程的堆栈
 };
 
-#define STACK_SIZE 4 * 1024 * 8 // uintptr_t ---->  8 in x86_64
-#define CANARY_SZ 2
-#define MAGIC 0x55
-struct co
-{
-	struct co *next;
-	void (*func)(void *);
-	void *arg;
-	char name[50];
-	enum co_status status;		   // 协程的状态
-	struct co *waiter;			   // 是否有其他协程在等待当前协程
-	jmp_buf context;			   // 寄存器现场 (setjmp.h)
-	uint8_t stack[STACK_SIZE + 1]; // 协程的堆栈						   // uint8_t == unsigned char
-};
-// void canary_init(uint8_t ptr[])
-// {
-// 	for (int i = 0; i < CANARY_SZ; i++)
-// 		ptr[i] = ptr[STACK_SIZE - i] = MAGIC;
-// }
-// void canary_check(uint8_t ptr[], struct co *co)
-// {
-// 	for (int i = 0; i < CANARY_SZ; i++)
-// 	{
-// 		if (ptr[i] != MAGIC)
-// 			printf("\n %s overflow", co->name);
-// 		if (ptr[STACK_SIZE - i] != MAGIC)
-// 			printf("\n %s overflow", co->name);
-// 	}
-// }
-struct co *current;
-struct co *co_start(const char *name, void (*func)(void *), void *arg)
-{
-	struct co *start = (struct co *)malloc(sizeof(struct co));
-	start->arg = arg;
-	start->func = func;
-	start->status = CO_NEW;
-	strcpy(start->name, name);
-	if (current == NULL) // init main
-	{
-		current = (struct co *)malloc(sizeof(struct co));
-		current->status = CO_RUNNING; // BUG !! 写成了 current->status==CO_RUNNING;
-		current->waiter = NULL;
-		strcpy(current->name, "main");
-		current->next = current;
-	}
-	//环形链表
-	struct co *h = current;
-	while (h)
-	{
-		if (h->next == current)
-			break;
+// 全局指针，指向当前运行的协程
+struct co* current = NULL;
 
-		h = h->next;
-	}
-	assert(h);
-	h->next = start;
-	start->next = current;
-	return start;
-}
-int times = 1;
-void co_wait(struct co *co)
-{
-	current->status = CO_WAITING;
-	co->waiter = current;
-	while (co->status != CO_DEAD)
-	{
-		co_yield ();
-	}
-	current->status = CO_RUNNING;
-	struct co *h = current;
+struct co* costack[MAX_CO]; // 存放所有的协程
+int co_num = 0; // 当前协程的数量
 
-	while (h)
-	{
-		if (h->next == co)
-			break;
-		h = h->next;
-	}
-	//从环形链表中删除co
-	h->next = h->next->next;
-	free(co);
+void append(struct co* co) {
+    assert(co_num < MAX_CO);
+    costack[co_num++] = co;
 }
-void co_yield ()
-{
-	if (current == NULL) // init main
-	{
-		current = (struct co *)malloc(sizeof(struct co));
-		current->status = CO_RUNNING;
-		strcpy(current->name, "main");
-		current->next = current;
-	}
-	assert(current);
-	int val = setjmp(current->context);
-	if (val == 0) // co_yield() 被调用时，setjmp 保存寄存器现场后会立即返回 0，此时我们需要选择下一个待运行的协程 (相当于修改 current)，并切换到这个协程运行。
-	{
-		// choose co_next
-		struct co *co_next = current;
-		do
-		{
-			co_next = co_next->next;
-		} while (co_next->status == CO_DEAD || co_next->status == CO_WAITING);
-		current = co_next;
-		if (co_next->status == CO_NEW)
-		{
-			assert(co_next->status == CO_NEW);
-			((struct co volatile *)current)->status = CO_RUNNING; //  fogot!!!
-			stack_switch_call(&current->stack[STACK_SIZE], current->func, (uintptr_t)current->arg);
-			((struct co volatile *)current)->status = CO_DEAD;
-			if (current->waiter)
-				current = current->waiter;
-		}
-		else
-		{
-			longjmp(current->context, 1);
-		}
-	}
-	else // longjmp returned(1) ,don't need to do anything
-	{
-		return;
-	}
+void delete(struct co* co) {
+    for (int i = 0; i < co_num; i++) {
+        if (costack[i] == co) {
+            for (int j = i; j < co_num - 1; j++) {
+                costack[j] = costack[j + 1];
+            }
+            co_num--;
+            return;
+        }
+    }
+}
+
+void __attribute__((constructor)) co_init() {
+    debug("co_init\n");
+    current = (struct co *)malloc(sizeof(struct co));
+    assert(current != NULL);
+    current->name = "main";
+    current->status = CO_RUNNING;
+    current->func = NULL;
+    current->arg = NULL;
+    append(current);
+}
+
+void co_wrapper(struct co* co) {
+    co->func(co->arg);
+    co->status = CO_DEAD;
+    delete(co);
+}
+
+struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+    struct co *co = (struct co *)malloc(sizeof(struct co));
+    assert(co != NULL);
+    co->name = strdup(name);
+    co->func = func;
+    co->arg = arg;
+    co->status = CO_NEW; // 新创建的协程
+    append(co);
+    return co;
+}
+
+void co_wait(struct co *co) { // 当前协程需要等待 co 执行完成
+    while (co->status != CO_DEAD) {
+        co_yield();
+    }
+    free(co);
+}
+
+void co_yield() {
+    int val = setjmp(current->context);
+    if (val == 0) {
+        int next = rand() % co_num;
+        struct co *next_co = costack[next];
+        if (next_co->status == CO_DEAD) {
+            return;
+        }
+        current = next_co;
+        current->status = CO_RUNNING;
+        stack_switch_call(current->stack + STACK_SIZE, co_wrapper, (uintptr_t)current);
+    } else {
+        return;
+    }
 }
