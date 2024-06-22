f58927a323b9c0bbc28c2ee5a92d13b265df65a0
tracer-nju <tracer@nju.edu.cn>
2024-06-22 20:38:19 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:38:19 up  1:58,  1 user,  load average: 0.12, 0.17, 0.17

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -146,26 +146,27 @@ void co_wait(struct co *co) { // 当前协程需要等待 co 执行完成
     }
 }
 
-void co_wrapper(struct co *co) {
-    co->func(co->arg);
+static void co_finish() {
+    current->status = CO_DEAD;
+    if (current->waiter != NULL) {
+        current = current->waiter;
+        longjmp(current->context, 0);
+    } else {
+        struct co *next = choose();
+        current = next;
+        if (current->status == CO_NEW) {
+            next->status = CO_RUNNING;
+            stack_switch_call(&current->stack[STACK_SIZE], co_wrapper, (uintptr_t)current);
+        } else {
+            longjmp(current->context, 1);
+        }
+    }
 }
 
-// static void co_finish() {
-//     current->status = CO_DEAD;
-//     if (current->waiter != NULL) {
-//         current = current->waiter;
-//         longjmp(current->context, 0);
-//     } else {
-//         struct co *next = choose();
-//         current = next;
-//         if (current->status == CO_NEW) {
-//             next->status = CO_RUNNING;
-//             stack_switch_call(&current->stack[STACK_SIZE], co_wrapper, (uintptr_t)current);
-//         } else {
-//             longjmp(current->context, 1);
-//         }
-//     }
-// }
+void co_wrapper(struct co *co) {
+    co->func(co->arg);
+    co_finish();
+}
 
 void co_yield() {
     int val = setjmp(current->context);
@@ -176,7 +177,6 @@ void co_yield() {
             next->status = CO_RUNNING;
             stack_switch_call(&current->stack[STACK_SIZE], co_wrapper, (uintptr_t)current);
         } else {
-            //! debug("co_yield: %s\n", current->name);
             longjmp(current->context, 1);
         }
     } else {
