1670afe794e046eb829323a9d099e9fa75f29977
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-06-22 15:47:06 +0800

    modify libco

--- libco/co.c
+++ libco/co.c
@@ -7,21 +7,20 @@
 #include <assert.h>
 #include <time.h>
 
-// #ifdef LOCAL_MACHINE
-//     #define debug(...) printf(__VA_ARGS__)
-// #else
-//     #define debug(...)
-// #endif
+#define DEBUG
+#ifdef DEBUG
+#define debug(...) printf(__VA_ARGS__)
+#else
 #define debug(...)
+#endif
 
-#define STACK_SIZE 64 * 1024
+#define STACK_SIZE (1 << 16)
 #define MAX_CO 150
 
 enum co_status {
-    CO_NEW = 1, // 新创建，还未执行过
-    CO_RUNNING, // 已经执行过
-    CO_WAITING, // 在 co_wait 上等待
-    CO_DEAD,    // 已经结束，但还未释放资源
+    CO_NEW,
+    CO_RUNNING,
+    CO_DEAD
 };
 
 static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
@@ -43,227 +42,68 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
 
 struct co {
     char *name;
-    void (*func)(void *); // co_start 指定的入口地址和函数
-    void *arg;
+    void (*func)(void *); // coroutine function
+    void *arg; // coroutine function argument
 
     enum co_status status;
-    struct co* waiter; // 是否有其他协程在等待当前协程，所以co->waiter = current
-    jmp_buf context; // 寄存器现场
-    uint8_t stack[STACK_SIZE]; // 协程的堆栈
-};
+    jmp_buf ctx;
+    uint8_t stack[STACK_SIZE];
+} ;
 
-// 全局指针，指向当前运行的协程
-struct co* current = NULL;
+struct co *current = NULL;
 
-// 需要用一个数据结构存储所有的协程
-typedef struct co_node {
-    struct co *ptr;
-    struct co_node *next;
-} co_node; 
-
-co_node *head = NULL; // 单向循环链表
-co_node *tail = NULL;
+struct co_stack {
+    struct co *q[MAX_CO];
+    int size;
+} co_stack;
 
 void append(struct co *co) {
-    co_node *node = (co_node *)malloc(sizeof(co_node));
-    assert(node != NULL);
-    // debug("append: %s\n", co->name);
-    node->ptr = co;
-    node->next = NULL;
-
-    if (head == NULL) {
-        head = node;
-        tail = node;
-        node->next = head;
-    } else {
-        tail->next = node;
-        node->next = head;
-        tail = node;
-    }
+    co_stack.q[co_stack.size] = co;
+    co_stack.size++;
 }
 
-void delete(struct co *co) { // 仅从链表删除，空间释放不在这里
-    co_node *node = head;
-    co_node *prev = NULL;
-    while (node != NULL) {
-        if (node->ptr == co) {
-            if (node == head) {
-                head = head->next;
-                if (head == NULL) tail = NULL;
-                else tail->next = head;
-            } else {
-                prev->next = node->next;
-                if (node == tail) tail = prev;
+void delete(struct co *co) {
+    for (int i = 0; i < co_stack.size; i++) {
+        if (co_stack.q[i] == co) {
+            for (int j = i; j < co_stack.size - 1; j++) {
+                co_stack.q[j] = co_stack.q[j + 1];
             }
-            free(node);
-            break;
+            co_stack.size--;
+            return;
         }
-        prev = node;
-        node = node->next;
     }
-    
 }
 
 void __attribute__((constructor)) co_init() {
-    if (current == NULL) { // 第一个协程，其实这个就该是main函数
-        current = (struct co *)malloc(sizeof(struct co));
-        current->status = CO_RUNNING;
-        current->waiter = NULL;
-        current->name = "main";
-        current->func = NULL;
-        current->arg = NULL;
-        append(current);
-    }
+    current = co_start("main", NULL, NULL);
+    current->status = CO_RUNNING;
 }
 
-void co_wrapper(struct co* co) {
+void co_wrapper(struct co *co) {
     co->func(co->arg);
-    co->status = CO_DEAD;
-    if (current->waiter != NULL) {
-        current = current->waiter;
-    }
+    co->status = CO_DEAD; // 运行完就可以删了
+    delete(co);
+    co_yield();
 }
 
-// 从头到尾，同时只有一个函数在被使用
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-    // 创建一个新的状态机，仅此而已（堆栈和状态机保存在共享内存？）
+struct co* co_start(const char *name, void (*func)(void *), void *arg) {
     struct co *co = (struct co *)malloc(sizeof(struct co));
-    assert(co != NULL);
-
+    if (co == NULL) {
+        return NULL;
+    }
     co->name = strdup(name);
     co->func = func;
     co->arg = arg;
     co->status = CO_NEW;
-    co->waiter = NULL;
-    memset(&co->context, 0, sizeof(co->context));
-    memset(co->stack, 0, sizeof(co->stack));
-    debug("co_start: %s\n", co->name);
-
+    memset(co->stack, 0, STACK_SIZE);
     append(co);
     return co;
 }
 
-void co_wait(struct co *co) { // 当前协程需要等待 co 执行完成
-    if (strcmp(co->name, "main") == 0) return; // main 函数不需要等待
-    assert(co != NULL);
-    current->status = CO_WAITING;
-    co->waiter = current;
+void co_wait(struct co *co) {
 
-    while(co->status != CO_DEAD) { // 不断切换可执行的线程执行，直到 co 执行完成
-        debug("co_wait1: %s\n", co->name);
-        // printf("111\n");
-        co_yield();
-        // debug("1\n");
-    }
-    current->status = CO_RUNNING;
-
-    debug("co_wait: %s finished\n", co->name);
-    delete(co);
-    free(co->name);
-    free(co);
-}
-
-co_node *choose_next() {
-    co_node *node_next = head; // head 是 main
-
-    // srand(time(NULL));
-    // int random = rand() % 5;
-    // for (int i = 0; i < random; i++) { // 随机化初始点
-    //     node_next = node_next->next;
-    // }
-    // while (node_next->ptr->status == CO_DEAD || node_next->ptr->status == CO_WAITING) {
-    //     node_next = node_next->next;
-    // }
-
-    while (node_next->ptr != current) {
-        node_next = node_next->next;
-    }
-    assert(node_next->ptr == current);
-    do {
-        node_next = node_next->next;
-    } while (node_next->ptr->status == CO_DEAD || node_next->ptr->status == CO_WAITING);
-
-    return node_next;
 }
 
 void co_yield() {
-    assert(current != NULL);
-    // debug("current: %s\n", current->name);
-    // printf("current: %d\n", current->status);
-    if (current->status == CO_DEAD) { //! 这里注释掉也会卡在test1
-        delete(current);
-        free(current->name);
-        free(current);
-        current = NULL;
-        return;
-    }
-    // assert(current->status == CO_WAITING || current->status == CO_RUNNING);
-    // debug("into yield\n"); // 很明显的一个地方是，test2 consumer里面有调用co_yield，那就是哪里实现错误了
 
-    int val = setjmp(current->context);
-    // debug("val: %d\n", val);
-    // traverse();
-    // show_status();
-    if (val == 0) { // 选择下一个待运行的协程
-        co_node *node_next = choose_next();
-        // debug("choose finished: %s\n", node_next->ptr->name);
-        // 但是为什么会卡在这里
-        assert(val == 0);
-
-        current = node_next->ptr;
-
-        if (node_next->ptr->status == CO_NEW) {
-            ((struct co volatile*)current)->status = CO_RUNNING;
-
-            // stack_switch_call(&current->stack[STACK_SIZE], node_next->ptr->func, (uintptr_t)node_next->ptr->arg);
-            // current->status = CO_DEAD;
-
-            stack_switch_call(&current->stack[STACK_SIZE], co_wrapper, (uintptr_t)current);
-        } else {
-            longjmp(current->context, 1);
-        }
-    } else {
-        return;
-    }
 }
-
-// // 遍历当前的链表s，这下链表终于好了
-// void traverse() {
-//     printf("\n------traverse------\n");
-//     co_node *node = head;
-//     do {
-//         printf("%s -> ", node->ptr->name);
-//         node = node->next;
-//     } while (node != tail);
-//     printf("%s\n", node->ptr->name);
-//     printf("--------------------\n");
-// }
-
-// void detect() {
-//     debug("------detect------\n");
-//     debug("current: %s\n", current->name);
-//     debug("head: %s\n", head->ptr->name);
-//     debug("tail: %s\n", tail->ptr->name);
-//     debug("------detect------\n");
-// }
-
-// void detect2() {
-//     debug("------detect2------\n");
-//     debug("is head->next == thd1: %d\n", head->next == tail);
-//     debug("------detect2------\n");
-// }
-
-// void detect3() {
-//     debug("------detect3------\n");
-//     debug("is head->next == thd2: %d\n", head->next->next == tail);
-//     debug("------detect3------\n");
-// }
-
-// void show_status() {
-//     co_node *node = head;
-//     do {
-//         debug("%s: %d\n", node->ptr->name, node->ptr->status);
-//         node = node->next;
-//     } while (node != tail);
-//     debug("%s: %d\n", node->ptr->name, node->ptr->status);
-// }
