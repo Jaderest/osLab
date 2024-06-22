7c3b3bd06fab48655af5febd5f1fa7a6d9e86965
tracer-nju <tracer@nju.edu.cn>
2024-06-22 19:05:44 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:05:44 up 26 min,  1 user,  load average: 0.21, 0.13, 0.05

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -120,7 +120,7 @@ void co_yield() {
         current = next_co;
         current->status = CO_RUNNING;
         debug("co_yield, switch to %s\n", current->name);
-        stack_switch_call(current->stack + STACK_SIZE, co_wrapper, (uintptr_t)current);
+        stack_switch_call(&current->stack[STACK_SIZE], co_wrapper, (uintptr_t)current);
     } else {
         return;
     }
