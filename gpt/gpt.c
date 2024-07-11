0c715cf682e8aa947da37dd0d02fa1a98ad7a8e7
Siyuan Jiang <221840206@smail.nju.edu.cn>
2024-07-11 16:30:42 +0800

    do somthing before

--- gpt/gpt.c
+++ gpt/gpt.c
@@ -85,20 +85,6 @@ void layernorm_forward(float* out, float* mean, float* rstd,
     }
 }
 
-struct matmul_args {
-    float* out;
-    float* inp;
-    float* weight;
-    float* bias;
-    int B;
-    int T;
-    int C;
-    int OC;
-    int start;
-    int end;
-};
-
-//TODO：反正优化这里就行了
 //matrix multiply
 void matmul_forward(float* out,
                     float* inp, float* weight, float* bias,
