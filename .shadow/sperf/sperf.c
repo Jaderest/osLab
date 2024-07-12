c100afa823d2c67399c2458c277b6158aebf4e1c
tracer-nju <tracer@nju.edu.cn>
2024-07-13 01:29:35 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:29:35 up  8:45,  1 user,  load average: 0.15, 0.09, 0.04

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -139,7 +139,7 @@ int main(int argc, char *argv[], char *envp[]) { // 参数存在argv中
                 debug("%f  %s  %f\n", start_time, name, syscall_time);
                 if (st == 0.0f) {
                     st = start_time;
-                } else if (start_time - st > 0.1f) {
+                } else if (start_time - st > 0.05f) {
                     print_top_syscalls(&syscall_array, 5);
                     free_syscall_array(&syscall_array);
                     st = 0.0f;
