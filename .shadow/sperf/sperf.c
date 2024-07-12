67c695993f433da7c1ccec7927c6300082d25220
tracer-nju <tracer@nju.edu.cn>
2024-07-13 00:05:38 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:05:38 up  7:21,  1 user,  load average: 0.15, 0.09, 0.03

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -37,6 +37,8 @@ int main(int argc, char *argv[], char *envp[]) { // 参数存在argv中
         //TODO 子进程
         close(pipefd[0]); // close read end
         dup2(pipefd[1], STDERR_FILENO); // redirect stderr to pipe，即将strace的输出重定向过去
+        int fd = open("/dev/null", O_WRONLY);
+        dup2(fd, STDOUT_FILENO); // redirect stdout to /dev/null
         // TODO：准备一下argv
 
         debug("execve(\"/usr/bin/strace\", argv, envp);\n");
