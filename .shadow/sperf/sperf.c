9caf61978449ace147f9626540aa275089be4bbf
tracer-nju <tracer@nju.edu.cn>
2024-07-13 16:47:58 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:47:58 up 10:29,  1 user,  load average: 0.08, 0.03, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -138,7 +138,7 @@ int main(int argc, char *argv[], char *envp[]) { // 参数存在argv中
         }
         char buf[1024];
         while (fgets(buf, sizeof(buf), fdopen(pipefd[0], "r")) != NULL) {
-            printf("%s", buf); // 奇怪啊
+            // printf("%s", buf); // 奇怪啊
             if (regexec(&regex, buf, 4, matchs, 0) == 0) {
                 buf[matchs[1].rm_eo] = '\0';
                 buf[matchs[2].rm_eo] = '\0';
