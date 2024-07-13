1d7b8d27f43886bad2847fe29ff2d026e7d67b66
tracer-nju <tracer@nju.edu.cn>
2024-07-14 01:11:18 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:11:18 up 14:54,  1 user,  load average: 0.09, 0.10, 0.08

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -47,8 +47,24 @@ static void *kalloc(size_t size) {
         return NULL;
     } else if (size < 16) { // 最小单元16字节
         size = 16;
-    } else if (size > PAGE_SIZE) { 
+    } else if (size > MAX_SIZE) { 
         return NULL;
+    } else if (size > PAGE_SIZE) { // 16KiB ~ 16MiB
+        size_t align = PAGE_SIZE;
+        while (align < size) {
+            align *= 2;
+        }
+        size = align;
+        debug("size: %d\n", size); //TODO: 我的klib要实现一下%ld
+        int offset = 0;
+        while (offset <= right) {
+            offset += size;
+        }
+        right = offset;
+        void *ret = NULL;
+        ret = heap.end - offset;
+        unlock(&pmm_lock);
+        return ret;
     } else { // 16 ~ 16KiB
         size_t align = 16;
         while (align < size) {
@@ -56,19 +72,18 @@ static void *kalloc(size_t size) {
         }
         size = align;
         debug("size: %d\n", size); //TODO: 我的klib要实现一下%ld
+        int offset = 0;
+        while (offset <= left) {
+            offset += size;
+        }
+        left = offset;
+        void *ret = NULL;
+        ret = heap.start + offset;
+        unlock(&pmm_lock);
+        return ret;
     }
-    int offset = 0;
-    while (offset <= left) {
-        offset += size;
-    }
-    left = offset;
 
-    void *ret = NULL;
-    //TODO: 实现指针偏移
-    ret = heap.start + offset;
-    unlock(&pmm_lock);
-
-    return ret;
+    return NULL;
 }
 
 static void kfree(void *ptr) {
