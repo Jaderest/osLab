23da5d9a26238fdac96bc4b8ac9e4d3c45498845
tracer-nju <tracer@nju.edu.cn>
2024-07-14 20:09:28 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:09:28 up  4:36,  1 user,  load average: 0.24, 0.12, 0.09

--- .shadow/kernel/src/pmm.h
+++ .shadow/kernel/src/pmm.h
@@ -1,5 +1,31 @@
 #ifndef _PMM_H
 #define _PMM_H
 
+// #include <common.h>
+#include "list.h"
+#include "thread.h"
+
+#define PAGE_SIZE 4096
+#define MIB (1024 * 1024)
+
+// slab allocator
+typedef struct object {
+    struct object *next;
+} object_t;
+
+typedef struct slab {
+    struct slab *next;       // 指向下一个slab
+    object_t *free_objects; // 指向空闲对象链表
+    size_t num_free; // 空闲对象数量
+    lock_t lock; // 用于保护该slab的锁
+    size_t size; // 每个对象的大小
+} slab_t;
+
+// slab分配器(链表数组)
+typedef struct cache {
+    slab_t *slabs; // 指向slab链表
+    size_t object_size; // 每个对象的大小
+    lock_t cache_lock; // 用于保护该cache的锁
+} cache_t;
 
 #endif
