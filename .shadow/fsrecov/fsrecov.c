c8f002d271b2da6b54918e012f1fe0922fb5f9be
tracer-nju <tracer@nju.edu.cn>
2024-07-19 17:00:22 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:00:22 up 1 day, 18:09,  1 user,  load average: 0.14, 0.13, 0.09

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -11,6 +11,13 @@
 #define SECTOR_SIZE 512
 #define CLUSTER_SIZE 4096 // 8 sectors
 
+#define DEBUG
+#ifdef DEBUG
+#define debug(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+#else
+#define debug(fmt, ...)
+#endif
+
 void *map_disk_image(const char *path, size_t *size) {
   int fd = open(path, O_RDONLY);
   if (fd < 0) {
@@ -49,11 +56,20 @@ void scan_clusters(const void *disk_image, size_t image_size, size_t cluster_siz
 int main(int argc, char *argv[]) {
   size_t image_size;
   void *disk_image = map_disk_image(argv[1], &image_size); //映射文件+获取文件大小
-
   struct fat32hdr *hdr = (struct fat32hdr *)disk_image;
   assert(hdr->Signature_word == 0xaa55);
+  debug("Volume ID: %u\n", hdr->BS_VolID);
+  debug("Volume Label: %s\n", hdr->BS_VolLab);
+  debug("File System Type: %s\n", hdr->BS_FilSysType);
+  debug("img size: %lu\n", image_size);
   // size_t cluster_size = CLUSTER_SIZE;
   // scan_clusters(disk_image, image_size, cluster_size);
   // munmap(disk_image, image_size);
+
+  void *cluster_addr = (void *)((uintptr_t)hdr + (hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_TotSec32 + hdr->BPB_HiddSec) * hdr->BPB_BytsPerSec);
+  uintptr_t addr = (uintptr_t)cluster_addr; // cluster_addr是void*类型，不能直接加减
+
+  
+
   return 0;
 }
