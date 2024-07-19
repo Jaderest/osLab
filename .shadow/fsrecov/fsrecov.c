3469fc3bbabf761090fc637f8639a642e57dcc3a
tracer-nju <tracer@nju.edu.cn>
2024-07-19 15:58:15 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:58:15 up 1 day, 17:06,  1 user,  load average: 0.17, 0.13, 0.09

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -27,24 +27,33 @@ void *map_disk_image(const char *path, size_t *size) {
   return disk_image;
 }
 
-void scan_clusters(const void *disk_image, size_t image_size,
-                   size_t cluster_size) {
+void scan_clusters(const void *disk_image, size_t image_size, size_t cluster_size) {
+  // 其实这里不止这个，还要恢复文件名，所以扫描clusters这里还要分类
+  /**
+   * 目录文件，以此恢复文件名（注意还得将它们文件名转化称ascii）
+   * BMP文件头部
+   * BMP实际数据
+   * 未使用的cluster
+   */
   size_t cluster_count = image_size / cluster_size;
 
   for (size_t cluster_num = 0; cluster_num < cluster_count; cluster_count++) {
     void *cluster_data = (u8 *)disk_image + cluster_num * cluster_size;
     struct BmpHeader *bmp_hdr = (struct BmpHeader *)cluster_data;
     if (bmp_hdr->bfType == 0x4d42) { // "BM"
-      // TODO：恢复文件，然后调用sha1sum
+      // TODO：恢复文件，文件名，然后调用sha1sum
     }
   }
 }
 
 int main(int argc, char *argv[]) {
   size_t image_size;
-  void *disk_image = map_disk_image(argv[1], &image_size);
-  size_t cluster_size = CLUSTER_SIZE;
-  scan_clusters(disk_image, image_size, cluster_size);
-  munmap(disk_image, image_size);
+  void *disk_image = map_disk_image(argv[1], &image_size); //映射文件+获取文件大小
+
+  struct fat32hdr *hdr = (struct fat32hdr *)disk_image;
+  assert(hdr->Signature_word == 0xaa55);
+  // size_t cluster_size = CLUSTER_SIZE;
+  // scan_clusters(disk_image, image_size, cluster_size);
+  // munmap(disk_image, image_size);
   return 0;
 }
