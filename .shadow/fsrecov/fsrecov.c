a0e1229a3bc4ac3db79d9145beab68b7460a6806
tracer-nju <tracer@nju.edu.cn>
2024-07-22 05:01:22 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:01:22 up 1 day,  1:32,  1 user,  load average: 0.12, 0.10, 0.09

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -57,8 +57,8 @@ void parse_bmp(struct BmpHeader *hdr, const char *name, const char *tmp_path) {
     // debug("BMP file: %s\t", name);
     // debug("File size: %u\n", hdr->bfSize);
     char file_name[512];
-    debug("tmp_path: %s\n", tmp_path);
-    // snprintf(file_name, sizeof(file_name), "%s%s.bmp", name);
+    // debug("tmp_path: %s\n", tmp_path);
+    snprintf(file_name, sizeof(file_name), "%s/%s.bmp", tmp_path, name);
   }
 }
 
