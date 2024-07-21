278d1b41711f15cc358a9f220a49cab54bdc4215
tracer-nju <tracer@nju.edu.cn>
2024-07-21 21:20:14 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:20:14 up 17:51,  1 user,  load average: 0.18, 0.12, 0.08

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -19,22 +19,38 @@
 #define debug(fmt, ...)
 #endif
 
-// void *map_disk_image(const char *path, size_t *size) {
-//   int fd = open(path, O_RDONLY);
-//   if (fd < 0) {
-//     perror("open");
-//     exit(EXIT_FAILURE);
-//   }
-//   *size = lseek(fd, 0, SEEK_END);
-//   void *disk_image = mmap(NULL, *size, PROT_READ, MAP_PRIVATE, fd, 0);
-//   if (disk_image == MAP_FAILED) {
-//     perror("mmap");
-//     return NULL;
-//   }
-//   close(fd);
-//   return disk_image;
-// }
+void *map_disk_image(const char *path, size_t *size);
 
 int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <disk_image>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  size_t img_size;
+  void *disk_img = map_disk_image(argv[1], &img_size);
+  size_t cluster_size = CLUSTER_SIZE;
+
+  struct fat32hdr *hdr = (struct fat32hdr *)disk_img;
+  debug("BPB_BytsPerSec: %d\n", hdr->BPB_BytsPerSec);
+  debug("BPB_SecPerClus: %d\n", hdr->BPB_SecPerClus);
+  debug("BPB_RsvdSecCnt: %d\n", hdr->BPB_RsvdSecCnt);
+  debug("BPB_NumFATs: %d\n", hdr->BPB_NumFATs);
+  debug("BPB_FATSz32: %d\n", hdr->BPB_FATSz32);
   return 0;
+}
+
+void *map_disk_image(const char *path, size_t *size) {
+  int fd = open(path, O_RDONLY);
+  if (fd < 0) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+  *size = lseek(fd, 0, SEEK_END);
+  void *disk_image = mmap(NULL, *size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (disk_image == MAP_FAILED) {
+    perror("mmap");
+    return NULL;
+  }
+  close(fd);
+  return disk_image;
 }
