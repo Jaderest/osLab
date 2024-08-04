7943dd20abb067c5816b5db20b50e62bf97403c4
tracer-nju <tracer@nju.edu.cn>
2024-08-05 01:02:17 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:02:17 up  2:05,  1 user,  load average: 0.23, 0.28, 0.21

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -15,8 +15,8 @@
 #define INT_MAX (0x7fffffff)
 #define INT_MIN (0x80000000)
 
-#define INTR assert(ienabled())
-#define NO_INTR assert(!ienabled())
+#define INTR PANIC_ON(!ienabled(), "Interrupt is disabled")
+#define NO_INTR PANIC_ON(ienabled(), "Interrupt is enabled")
 
 
 //------------------log------------------
