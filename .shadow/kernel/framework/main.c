f827b463c5637a5266d2c1360bfe3803978867f9
tracer-nju <tracer@nju.edu.cn>
2024-08-05 23:17:50 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:17:50 up  5:42,  1 user,  load average: 0.18, 0.80, 1.04

--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -21,27 +21,27 @@ void alignTest() {
     while (1) ;
 }
 
-// sem_t empty, fill;
-// static void producer(void *arg) {
-//     while (1) {
-//         kmt->sem_wait(&empty);
-//         printf("(");
-//         kmt->sem_signal(&fill);
-//     }
-// }
-// static void consumer(void *arg) {
-//     while (1) {
-//         kmt->sem_wait(&fill);
-//         printf(")");
-//         kmt->sem_signal(&empty);
-//     }
-// }
-// static void create_threads() {
-//     kmt->sem_init(&empty, "empty", 10);
-//     kmt->sem_init(&fill, "fill", 0);
-//     kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
-//     kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
-// }
+sem_t empty, fill;
+static void producer(void *arg) {
+    while (1) {
+        kmt->sem_wait(&empty);
+        printf("(");
+        kmt->sem_signal(&fill);
+    }
+}
+static void consumer(void *arg) {
+    while (1) {
+        kmt->sem_wait(&fill);
+        printf(")");
+        kmt->sem_signal(&empty);
+    }
+}
+static void create_threads() {
+    kmt->sem_init(&empty, "empty", 10);
+    kmt->sem_init(&fill, "fill", 0);
+    kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
+    kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
+}
 
 
 int main() {
@@ -50,7 +50,7 @@ int main() {
     log("before os_init\n");
     os->init();
     log ("init done\n");
-    // create_threads();
+    create_threads();
 
     // 所有处理器运行同一份代码，拥有独立的堆栈，共享的内存
     mpe_init(os->run); // 让每个处理器都运行os->run，此时操作系统真正化身成了中断处理程序
