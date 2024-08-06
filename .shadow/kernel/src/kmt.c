7a07294bd256e16c83895b7db3bcaef040b6b5ff
tracer-nju <tracer@nju.edu.cn>
2024-08-07 02:17:19 +0800

    Linux Jaderest-win 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:17:19 up  8:33,  1 user,  load average: 0.16, 0.24, 0.20

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -53,7 +53,7 @@ Context *kmt_schedule(Event ev, Context *ctx) { // ?理一下思路先，不急�
     } else {
         current = tasks[index];
         current->status = RUNNABLE;
-    }
+    } // return idle的时候发现stack overflow？
     PANIC_ON(stack_check(current), "stack overflow in cpu %d", cpu_current());
     return current->context;
 }
